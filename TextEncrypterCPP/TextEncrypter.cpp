6e5706581865f8b03e0236e5aecc97d8cfd6afb1
salaminpeter <salamin.peter@gmail.com>
2025-05-22 00:38:52 +0200

    add new cpp src files

---
+++ TextEncrypterCPP/TextEncrypter.cpp
@@ -0,0 +1,57 @@
+#include "TextEncrypter.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#include <iostream>
+
+bool CTextEncrypter::LoadImg(const char* path)
+{
+	int channels;
+
+	if (!stbi_info(path, &m_ImageWidth, &m_ImageHeight, &channels))
+		return false;
+
+	m_ImagePath = path;
+	size_t DataSize = m_ImageWidth * m_ImageHeight * max(3, channels);
+	m_ByteData.clear();
+	stbi_uc* data = stbi_load(path, &m_ImageWidth, &m_ImageHeight, &channels, 3);
+	m_ByteData.assign(data, data + DataSize);
+
+	stbi_image_free(data);
+
+	return true;
+}
+
+void CTextEncrypter::AddTextToImage(const char* text)
+{
+	m_CurrentByte = 0;
+	std::uint16_t l = strlen(text);
+	WriteToByteData(l);
+	const char* p = text;
+
+	while (*p)
+		WriteToByteData(*(p++));
+
+	//m_ImagePath = m_ImagePath.substr(0, m_ImagePath.find_last_of(".") + 1) + "bmp";
+	int i = std::remove(m_ImagePath.c_str());
+	stbi_write_png(m_ImagePath.c_str(), m_ImageWidth, m_ImageHeight, 3, &(m_ByteData[0]), m_ImageWidth * 3);
+}
+
+std::string CTextEncrypter::ReadTextFromImage()
+{
+	m_CurrentByte = 0;
+	std::uint16_t TextLength = ReadFromByteData<std::uint16_t>();
+	std::string res;
+
+	for (int i = 0; i < TextLength; ++i)
+	{
+		char c = ReadFromByteData<char>();
+		res += c;
+	}
+
+	return res;
+}
---
+++ TextEncrypterCPP/TextEncrypter.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <msclr/marshal_cppstd.h>
+
+class CTextEncrypter
+{
+public:
+	bool LoadImg(const char* path);
+
+	void AddTextToImage(const char* text);
+	std::string ReadTextFromImage();
+
+	void ResetCursor() { m_CurrentByte = 0; }
+
+	template <typename T>
+	bool WriteToByteData(T data)
+	{
+		if (m_CurrentByte + sizeof(T) * 8 >= m_ByteData.size())
+			return false;
+
+		
+		for (int j = 0; j < sizeof(T); ++j)
+		{
+			uint8_t CurrentData = (data >> (j * 8)) & 255;
+
+			for (int i = 0; i < 8; ++i)
+			{
+				T bit = ((CurrentData >> (7 - i)) & 1);
+
+				if (bit == 0)
+					m_ByteData[m_CurrentByte] &= (bit | 254);
+				else
+					m_ByteData[m_CurrentByte] |= (bit | 254);
+
+				++m_CurrentByte;
+			}
+		}
+	}
+
+	template <typename T>
+	T ReadFromByteData()
+	{
+		T res = 0;
+
+		for (int j = 0; j < sizeof(T); ++j)
+		{
+			for (int i = 0; i < 8; ++i)
+			{
+				T bit = m_ByteData[m_CurrentByte++] & 1;
+				T bit1 = bit << ((j * 8 + 7 - i));
+				res |= bit1;
+			}
+		}
+
+		return res;
+	}
+
+
+private:
+	std::vector<std::uint8_t> m_ByteData;
+	int m_ImageWidth;
+	int m_ImageHeight;
+	std::string m_ImagePath;
+	size_t m_CurrentByte = 0;
+};
